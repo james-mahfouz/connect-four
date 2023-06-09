23aabdd4a1f0dab16839bc6da1cf409a7f99ef51
James Mahfouz <jamesmahfouz0@gmail.com>
2023-06-09 18:47:13 +0300

    added routing to change names

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import Board from './components/Board'
+import { useState } from "react";
+import Board from "./components/Board";
+import PlayerName from "./components/PlayerNames";
 
-import './App.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
-
-  return <Board />
+  return (
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Board />} />
+          <Route path="/name" element={<PlayerName />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/PlayerNames/index.tsx
@@ -0,0 +1,9 @@
+const PlayerName: React.FC = () => {
+  return (
+    <div className="game-body">
+      <div className="player-names"></div>
+    </div>
+  );
+};
+
+export default PlayerName;
